4f578f5e26ce56bdc91659383330574a06c1a040
Sam Kim <skim13@cs.stanford.edu>
2022-05-26 14:05:30 +0900

    edits

--- minting-dapp/src/scripts/lib/NftContractType.ts
+++ minting-dapp/src/scripts/lib/NftContractType.ts
@@ -1,7 +1,7 @@
-// The name below ("MetatsuitHBv1") should match the name of your Solidity contract.
+// The name below ("MetatsuitBonzai") should match the name of your Solidity contract.
 // It can be updated using the following command:
 // yarn rename-contract NEW_CONTRACT_NAME
 // Please DO NOT change it manually!
-import { MetatsuitHBv1 as NftContractType } from '../../../../smart-contract/typechain/index';
+import { MetatsuitBonzai as NftContractType } from '../../../../smart-contract/typechain/index';
 
 export default NftContractType;
--- smart-contract/lib/NftContractProvider.ts
+++ smart-contract/lib/NftContractProvider.ts
@@ -1,8 +1,8 @@
-// The name below ("MetatsuitHBv1") should match the name of your Solidity contract.
+// The name below ("MetatsuitBonzai") should match the name of your Solidity contract.
 // It can be updated using the following command:
 // yarn rename-contract NEW_CONTRACT_NAME
 // Please DO NOT change it manually!
-import { MetatsuitHBv1 as ContractType } from '../typechain/index';
+import { MetatsuitBonzai as ContractType } from '../typechain/index';
 
 import { ethers } from 'hardhat';
 import CollectionConfig from './../config/CollectionConfig';
