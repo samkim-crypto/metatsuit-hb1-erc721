34888eeae3371fde8c306862f6547e3230e42923
Sam Kim <skim13@cs.stanford.edu>
2022-05-30 21:32:53 +0900

    edits

--- smart-contract/config/CollectionConfig.ts
+++ smart-contract/config/CollectionConfig.ts
@@ -9,15 +9,15 @@ const CollectionConfig: CollectionConfigInterface = {
   // The contract name can be updated using the following command:
   // yarn rename-contract NEW_CONTRACT_NAME
   // Please DO NOT change it manually!
-  contractName: "MetatBonsaiTest12",
-  tokenName: "METATBONSAI Collection Test Version 12",
+  contractName: "MetatBonsaiTest14",
+  tokenName: "METATBONSAI Collection Test Version 14",
   tokenSymbol: "HBv1",
   hiddenMetadataUri:
-    "ipfs://QmYhKhLijRe8KiByc1DQJLiCUgTr31GHBs92fVWbH2yfVV/hidden.json",
+    "ipfs://QmXeZubposMnj4RnSsCogqsyCAq723Fr2pxic3EZCsDkge/hidden.json",
   maxSupply: 3000,
   whitelistSale: {
     price: 0.0,
-    maxMintAmountPerTx: 100,
+    maxMintAmountPerTx: 350,
   },
   preSale: {
     price: 0.01,
@@ -27,7 +27,7 @@ const CollectionConfig: CollectionConfigInterface = {
     price: 0.01,
     maxMintAmountPerTx: 100,
   },
-  contractAddress: "0x180D9b6820b748c6b9DD1D75a715Ac350571A7aB",
+  contractAddress: "0x0D23F75277D3920B67E76AA2fe55Eef0ff8bD147",
   marketplaceIdentifier: "my-nft-token",
   marketplaceConfig: Marketplaces.openSea,
   whitelistAddresses,
